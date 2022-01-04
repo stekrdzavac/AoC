cb253ef43a53df70141802207782965c051a4639
Stefan Krdzavac <stefankrdzavac@gmail.com>
2022-01-04 17:22:58 +0100

    Advent 2017 day 1.

--- 2017/day1/day1.go
+++ 2017/day1/day1.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 
@@ -23,5 +24,43 @@ func main() {
 	if err != nil {
 		fmt.Printf("got error: %+v", err)
 	}
+	fmt.Println("Part one =", partOne(input))
+	fmt.Println("Part two =", partTwo(input))
 
+}
+
+func partOne(input string) int {
+
+	input = input + string(input[0])
+	sum := 0
+	i := 0
+	for index := 0; index < len(input); {
+		for i = index + 1; i < len(input); i++ {
+			if (input[index] == input[i]) {
+				num, _ := strconv.Atoi(string(input[index]))
+				sum += num
+			} else {
+				break
+			}
+		}
+		index = i
+	}
+
+	return sum
+}
+
+func partTwo(input string) int {
+
+	sum := 0
+	step := len(input) / 2
+
+	for index := 0; index < len(input) / 2; index++ {
+		secondIndex := index + step
+		if input[index] == input[secondIndex] {
+			num, _ := strconv.Atoi(string(input[index]))
+			sum += num * 2
+		}
+	}
+
+	return sum
 }
